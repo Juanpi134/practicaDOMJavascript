4513d738574e1c51cac54632dec6a1dc8974869c
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-06 13:23:59 -0300

    se añade el archivo formulario.js en la carpeta js y se obtienen los datos

---
+++ js/formulario.js
@@ -0,0 +1,10 @@
+//Obtencion de los datos del formulario
+const formulario = document.querySelector("#formulario");
+
+const nombre = document.querySelector("#nombre");
+const edad = document.querySelector("#edad");
+const activo = document.querySelector("#activo");
+const pais = document.querySelector("#pais");
+
+
+console.log(edad);
