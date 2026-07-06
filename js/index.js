8d46a6203464b44fbb9f88461c5139c1a403d3d2
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 21:19:05 -0300

    se añade un ejemplo renderizando listas

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const lenguajes = ["HTML","CSS","JS","REACT"];
+
+//recorri cada lenguaje
+//HTML,
+// CSS,
+// JS,
+// REACT
+lenguajes.forEach((lenguaje) => {
+    console.log(lenguaje);
+})
+
+//obtuve el UL
+const ul = document.querySelector("ul");
+
+
+//acá lo que hice fue renderizar con map cada lenguaje le añadi un elemento para que se pueda copiar el contenido
+lenguajes.map((lenguaje) => {
+    const li = document.createElement("li");
+
+    li.textContent = lenguaje;
+
+   
+   return  ul.appendChild(li);
+
+});
+
+//finalmente retorne el ul
+document.body.appendChild(ul);
