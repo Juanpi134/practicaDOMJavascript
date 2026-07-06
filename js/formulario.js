228ff0dc7f298cb6b1d46ab0a9cd92e35c0d69ab
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-06 14:25:05 -0300

    se agregan nuevos ejemplos

--- js/formulario.js
+++ js/formulario.js
@@ -5,6 +5,54 @@ const nombre = document.querySelector("#nombre");
 const edad = document.querySelector("#edad");
 const activo = document.querySelector("#activo");
 const pais = document.querySelector("#pais");
-
+const button = document.querySelector("button");
 
 console.log(edad);
+
+// con esto puedo ocultar el input con hidden
+// edad.hidden = true;
+
+//console.log(nombre.value);
+
+//Verificar si esta activo o no el input
+console.log(activo.checked)
+
+
+//verificar la posicion del select
+console.log(pais.selectedIndex);
+
+//obtener todas las opciones
+//Devuelve un HTMLOptionsCollection
+console.log(pais.options);
+
+
+//se recorre cada opcion
+Array.from(pais.options).forEach(opcion => {
+    console.log(opcion.text);
+});
+
+
+//cambiar el texto de cada boton
+Array.from(pais.options).forEach(opcion => {
+    opcion.textContent = "Brasil";
+});
+
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+
+button.addEventListener("click", cambiarNombre());
+
+
+button.addEventListener("click", seleccionarCheckbox());
+
+
+function cambiarNombre(){
+    nombre.value = "Juan";
+}
+
+function seleccionarCheckbox(){
+    activo.checked = true;
+}
